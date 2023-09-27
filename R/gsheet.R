b50178324c439ad9194d807aa063893cf4b24dd1
Florian Oswald <florian.oswald@gmail.com>
2023-09-27 09:01:39 +0200

    2023 report

--- R/gsheet.R
+++ R/gsheet.R
@@ -1,8 +1,32 @@
 
-# All EJ related sheets
+# All related sheets
 
-sheet_url <- function() {"https://docs.google.com/spreadsheets/d/1D7nhTs8ao9yIW-PQ_z4zjYNB3pGMWcLWABtaA4I_WL0"}
+sheet_url   <- function() {"https://docs.google.com/spreadsheets/d/1D7nhTs8ao9yIW-PQ_z4zjYNB3pGMWcLWABtaA4I_WL0"}
+EctJsheet_url   <- function() {"https://docs.google.com/spreadsheets/d/1fCz0ZyK7Fsw2E9jbgKgedtoDgmBePk2V_0cuyHijfv0"}
+billing_url <- function(){"https://docs.google.com/spreadsheets/d/1dJijWplmvxlqgFgzYHy7gFs4FCFsLAv7LifX4g-4GIo"}
 
+
+#' Read EctJ sheet
+#'
+#'@export
+read_ectj <- function(refresh = FALSE){
+    if (refresh){
+        x = data.table(
+            googlesheets4::read_sheet(
+                EctJsheet_url(),sheet = "List",skip = 1, range = "List!A2:AD1000000") %>%
+                janitor::clean_names()
+        )
+        x = x[!is.na(ms)]
+        saveRDS(x, file = "~/Dropbox/EctJ/EctJ-1-key-documents/Report/current-sheet.Rds")
+    } else {
+        x = readRDS(file = "~/Dropbox/EctJ/EctJ-1-key-documents/Report/current-sheet.Rds")
+    }
+    x
+}
+
+#' Read EJ Google Sheet with Replications
+#'
+#' @export
 read_list <- function(refresh = FALSE){
     if (refresh){
         x = data.table(
@@ -24,7 +48,7 @@ clean_list <- function(refresh_sheet = FALSE){
 
     x[, completed_quarter := zoo::as.yearqtr(date_completed)]
     # create some variables
-    x[, completed := any(status %in% c("NT", "P", "p")), by = ms]
+    x[, completed := status %in% c("AP","NT", "P", "p"), by = ms]
     x[, hours_paper := sum(hours_spent), by = ms]
     x[, iterations_paper := max(round), by = ms]
 
@@ -39,10 +63,9 @@ clean_list <- function(refresh_sheet = FALSE){
     x[ , cumtime_resubmission := cumtime_decision + time_resubmission]
 
     # melt for plotting
-    mr = melt(r, id.vars = c("arrival_date","ms","round"),
-              measure.vars = c("time_assign","cumtime_replication","cumtime_decision"))
-    ggplot(mr, aes(x = arrival_date,y= value, color = variable)) + geom_point()
-
+    # mr = melt(r, id.vars = c("arrival_date","ms","round"),
+    #           measure.vars = c("time_assign","cumtime_replication","cumtime_decision"))
+    # ggplot(mr, aes(x = arrival_date,y= value, color = variable)) + geom_point()
 
 
     # moving averages at iteration level
@@ -51,61 +74,123 @@ clean_list <- function(refresh_sheet = FALSE){
     x[ , sround := round]
     x[sround > 2, sround := 3]
     hazard = x[, .(hazard = mean(status == "R")), by = .(arrival_date, sround)]
-    x[, hazard := mean(status == "R"), by = .(arrival_date, round)]
-    x[round == 1, fail1 := status == "R"]
-    x[round == 2, fail2 := status == "R"]
-    x[round > 2, fail3 := status == "R"]
+    hazard[, hazard_smooth := data.table::frollmean(hazard,n = 11,na.rm = TRUE), by = .(sround)]
 
-
-    x[, ma_time_assign := frollmean(time_assign,n = 11,na.rm = TRUE)]
-    x[, ma_time_replication := frollmean(cumtime_replication,n = 11,na.rm = TRUE)]
-    x[, ma_time_decision := frollmean(cumtime_decision,n = 11,na.rm = TRUE)]
-
-    # add total timing at paper level
-    xp = x[, c("time_assign_paper",
-          "time_replication_paper",
-          "time_decision_paper",
-          "time_resubmission_paper") := list(
-              sum(time_assign),
-              sum(time_replication),
-              sum(time_decision),
-              sum(time_resubmission)
+    # add total timing at completed paper level
+    xp <- x[ , list(
+              hours_spent = sum(hours_spent),
+              iterations_paper = .N,
+              time_assign_paper = sum(time_assign),
+              time_replication_paper =sum(cumtime_replication),
+              time_decision_paper = sum(cumtime_decision),
+              time_resubmission_paper = sum(cumtime_resubmission),
+              arrival_date = .SD[round == 1, as.Date(arrival_date)]
           ),
       by = ms]
-    # add cumulatives
-    xp[, cumtime_replication_paper := time_assign_paper + time_replication_paper]
-    xp[, cumtime_decision_paper := cumtime_replication_paper + time_decision_paper]
-    xp[, cumtime_resubmission_paper := cumtime_decision_paper + time_resubmission_paper]
+    # # add cumulatives
+    # xp[, cumtime_replication_paper := time_assign_paper + time_replication_paper]
+    # xp[, cumtime_decision_paper := cumtime_replication_paper + time_decision_paper]
+    # xp[, cumtime_resubmission_paper := cumtime_decision_paper + time_resubmission_paper]
 
 
-    x
+    x[, arrival_date := as.Date(arrival_date)]
+
+    list(iterations = x, papers = xp, iteration_hazard = hazard)
 }
-read_replicators <- function(){
-    x = googlesheets4::read_sheet(
-        sheet_url(),sheet = "Replicator-Availability")
-    x$replicator
+read_replicators <- function(refresh = FALSE){
+    if (refresh){
+        x = data.table(
+            googlesheets4::read_sheet(
+                sheet_url(),sheet = "Replicator-Availability")
+        )
+        saveRDS(x, file = "~/Dropbox/EJ/EJ-1-key-documents/Report/current-replicators.Rds")
+    } else {
+        x = readRDS(file = "~/Dropbox/EJ/EJ-1-key-documents/Report/current-replicators.Rds")
+    }
+    x
 }
 
 #' compute quarterly stats for each replicator
-billing <- function(x){
-    r = x[Checker %in% read_replicators()]
-    # throw out unfinished jobs
-    r = r[!is.na(completed_quarter)]
-    # compute stats by quarter
-    qtr = r[, .(N_packages = .N,
-                Avg_hours = mean(hours_spent,na.rm = TRUE),
-                Tot_hours = sum(hours_spent,na.rm = TRUE),
-                Avg_days = mean(`Days replic.`,na.rm = TRUE),
-                share_reject = mean(Decision == "R",na.rm = TRUE) ), by = .(Checker,completed_quarter)]
+#'
+billing <- function(rate = 25, write = FALSE,refresh = FALSE){
+    xectj = read_ectj(refresh = refresh)
+    xej = read_list(refresh = refresh)
 
-    # compute stats overall
-    ov =r[, .(N_packages = .N,
-            Avg_hours = mean(`Hours spent`,na.rm = TRUE),
-            Tot_hours = sum(`Hours spent`,na.rm = TRUE),
-            Avg_days = mean(`Days replic.`,na.rm = TRUE),
-            share_reject = mean(Decision == "R",na.rm = TRUE) ), by = Checker]
+    r0 = xectj[checker %in% read_replicators()$replicator][,
+                                                           .(checker,date_assigned,
+                                                             ms,
+                                                             date_completed,
+                                                             hours_spent,
+                                                             days_replic,
+                                                             decision,
+                                                             journal = "EctJ"
+                                                             )]
 
-    list(qtr,ov)
+
+    r1 = xej[checker %in% read_replicators()$replicator][,
+                                                        .(checker,date_assigned,
+                                                          ms,
+                                                          date_completed,
+                                                          hours_spent,
+                                                          days_replic,
+                                                          decision,
+                                                          journal = "EJ"
+                                                        )]
+
+    r = rbind(r0,r1)
+    r[, assigned_quarter := zoo::as.yearqtr(date_assigned)]
+
+    # r = r[!is.na(completed_quarter)]
+    # compute total payments by quarter
+    qtr = r[, .(completed_jobs = sum(!is.na(date_completed)),
+                completed_papers = .SD[!is.na(date_completed), length(unique(ms))],
+                ongoing_jobs = sum(is.na(date_completed)),
+                Avg_hours = round(mean(hours_spent,na.rm = TRUE),2),
+                Tot_hours = round(sum(hours_spent,na.rm = TRUE),2),
+                qtr_payment_euros = round(sum(hours_spent,na.rm = TRUE) * rate,2),
+                Avg_days = round(mean(days_replic,na.rm = TRUE),2),
+                share_reject = round(mean(decision == "R",na.rm = TRUE) , 2)), by = .(checker,assigned_quarter)]
+
+    # compute total payments by quarter for EJ
+    qtr_ej = r[journal == "EJ", .(completed_jobs = sum(!is.na(date_completed)),
+                completed_papers = .SD[!is.na(date_completed), length(unique(ms))],
+                ongoing_jobs = sum(is.na(date_completed)),
+                Avg_hours = round(mean(hours_spent,na.rm = TRUE),2),
+                Tot_hours = round(sum(hours_spent,na.rm = TRUE),2),
+                qtr_payment_euros = round(sum(hours_spent,na.rm = TRUE) * rate,2),
+                Avg_days = round(mean(days_replic,na.rm = TRUE),2),
+                share_reject = round(mean(decision == "R",na.rm = TRUE) , 2)), by = .(checker,assigned_quarter)]
+    # compute total payments by quarter for EctJ
+    qtr_ectj = r[journal == "EctJ", .(completed_jobs = sum(!is.na(date_completed)),
+                                  completed_papers = .SD[!is.na(date_completed), length(unique(ms))],
+                                  ongoing_jobs = sum(is.na(date_completed)),
+                                  Avg_hours = round(mean(hours_spent,na.rm = TRUE),2),
+                                  Tot_hours = round(sum(hours_spent,na.rm = TRUE),2),
+                                  qtr_payment_euros = round(sum(hours_spent,na.rm = TRUE) * rate,2),
+                                  Avg_days = round(mean(days_replic,na.rm = TRUE),2),
+                                  share_reject = round(mean(decision == "R",na.rm = TRUE) , 2)), by = .(checker,assigned_quarter)]
+
+    # compute stats all times
+    ov =r[, .(completed_jobs = sum(!is.na(date_completed)),
+              completed_papers = .SD[!is.na(date_completed), length(unique(ms))],
+              ongoing_jobs = sum(is.na(date_completed)),
+              Avg_hours = round(mean(hours_spent,na.rm = TRUE),2),
+              Tot_hours = round(sum(hours_spent,na.rm = TRUE),2),
+              qtr_payment_euros = round(sum(hours_spent,na.rm = TRUE) * rate,2),
+              Avg_days = round(mean(days_replic,na.rm = TRUE),2),
+              share_reject = round(mean(decision == "R",na.rm = TRUE) , 2) ), by = checker]
+
+
+    if (write){
+        qtr[, assigned_quarter := as.character(assigned_quarter)]
+        qtr_ej[, assigned_quarter := as.character(assigned_quarter)]
+        qtr_ectj[, assigned_quarter := as.character(assigned_quarter)]
+        googlesheets4::write_sheet(qtr, billing_url(), sheet = "quarterly-payments-overall")
+        googlesheets4::write_sheet(qtr_ej, billing_url(), sheet = "quarterly-payments-EJ")
+        googlesheets4::write_sheet(qtr_ectj, billing_url(), sheet = "quarterly-payments-EctJ")
+    }
+    list(qtr,ov,qtr_ectj,qtr_ej)
+
 }
 
 
--- R/plots.R
+++ R/plots.R
@@ -1,5 +1,275 @@
 
 
-plot_proctime <- function(x){
+
+plot_all <- function(year = 2023, refresh = FALSE,write = FALSE){
+    # width and height
+    w1 = 8
+    h1 = 6
+
+    # width and height narrower
+    w2 = 5
+    h2 = 5
+
+    path = file.path("~","git","EJData","reports",paste0(year,"-Report"),"images")
+
+    li = read_list(refresh = refresh)
+    cl = clean_list(refresh = refresh)
+
+    out = list()
+    out$proctime_iters = plot_proctime(cl$iterations)
+    out$rejection_hazard = plot_hazard(cl$iteration_hazard)
+
+    tab = li[, .(iterations_paper = max(round)), by = ms][, .N,  by = iterations_paper]
+    out$iter_paper_bar = ggplot(tab, aes(x = iterations_paper, y = N)) +
+        ggplot2::geom_bar(stat="identity", fill = "black") + theme_bw()
+
+
+    out$proctime_papers = plot_proctime(cl$papers,by = "Paper")
+
+    hpi = plot_hours_iters_paper()
+    out$hours_iteration = hpi$hours_i
+    out$hours_paper = hpi$hours_p
+    out$iters_paper = hpi$iters_p
+
+    out$data_statements = plot_DS(li)
+
+    if (write){
+        ggplot2::ggsave(file.path(path,"proctime_iters.png"),
+                        plot = out$proctime_iters, width = w1, height=h1)
+        ggplot2::ggsave(file.path(path,"hazard.png"),
+                        plot = out$rejection_hazard, width = w1, height=h1)
+        ggplot2::ggsave(file.path(path,"iters_paper_bar.png"),
+                        plot = out$iter_paper_bar, width = w1, height=h1)
+
+        ggplot2::ggsave(file.path(path,"proctime_papers.png"),
+                        plot = out$proctime_papers, width = w1, height=h1)
+        ggplot2::ggsave(file.path(path,"hours_iter.png"),
+                        plot = out$hours_iteration, width = 7, height=h1)
+        ggplot2::ggsave(file.path(path,"hours_paper.png"),
+                        plot = out$hours_paper, width = 7, height=h1)
+        ggplot2::ggsave(file.path(path,"iters_paper.png"),
+                        plot = out$iters_paper, width = 7, height=h1)
+        ggplot2::ggsave(file.path(path,"datastatements.png"),
+                        plot = out$data_statements, width = w2, height=h2)
+    }
+
+    li[ , sround := round]
+    li[sround > 5, sround := 5]
+    tab = li[, .(iterations_paper = max(round)), by = ms][, prop.table(table(iterations_paper))]
+
+    out
+
+
+}
+
+plot_trends <- function(x){
+    n = x[, .(Papers = length(unique(ms)),Iterations = .N),
+          by = .(quarter = zoo::as.yearqtr(arrival_date))]
+
+    l = list()
+    l$p1 = ggplot(melt(n, id.vars = "quarter",value.name = "Count"), aes(x = quarter , y = Count )) +
+        geom_point() + geom_line() +
+        facet_wrap(~variable) + theme_bw()
+
+    n[ , Iterations_Paper := Iterations / Papers]
+    l$p2 = ggplot(n, aes(x = quarter , y = Iterations_Paper )) +
+        geom_point() + geom_line() + theme_bw()
+
+}
+
+#' give clean_list
+plot_hours_iters_paper <-function(){
+    x = clean_list()
+    stopifnot(is.list(x))
+    l = list()
+    ii = x$iterations[hours_spent < 6, .(hours_spent = median(hours_spent, na.rm= TRUE)), by = arrival_date]
+    ii[, hours_smooth := data.table::frollmean(hours_spent,n = 11,na.rm = TRUE)]
+
+    l$hours_i = ggplot(x$iterations[hours_spent < 6], aes(x = arrival_date, y = hours_spent)) + geom_point(size = 0.5) +
+        geom_line(data = ii, aes(x = arrival_date,y = hours_smooth), size = 1, color = "red") + theme_bw() +
+        scale_x_date("Arrival Date",breaks = seq(as.Date("2020-01-01"), as.Date("2023-10-01"), by = "6 months") ,date_labels = "%Y-%m",
+                     limits = c(as.Date("2020-01-01"), NA)) +
+        geom_vline(xintercept = as.Date("2023-07-01")) + ggtitle("Hours Per Iteration", subtitle = glue::glue("Dropping {nrow(x$iterations[hours_spent>=6])} Outliers"))
+
+    pp = x$papers[ hours_spent < 10,.(hours_spent = median(hours_spent, na.rm= TRUE)), by = arrival_date]
+    pp[, hours_spent := data.table::frollmean(hours_spent,n = 11,na.rm = TRUE)]
+
+    ip = x$papers[,.(iters_paper = median(iterations_paper, na.rm= TRUE)), by = arrival_date]
+    ip[, iterations_paper := data.table::frollmean(iters_paper,n = 11,na.rm = TRUE)]
+
+
+    l$hours_p = ggplot(x$papers[ hours_spent < 10], aes(x = arrival_date, y = hours_spent)) + geom_point(size = 0.5) +
+        geom_line(data = pp, aes(x = arrival_date, y = hours_spent), color = "red",size = 1) + theme_bw() +
+        scale_x_date("Arrival Date",limits = c(as.Date("2020-01-01"), NA), breaks = seq(as.Date("2020-01-01"), as.Date("2023-10-01"), by = "6 months") ,date_labels = "%Y-%m") +
+        geom_vline(xintercept = as.Date("2023-07-01")) + ggtitle("Hours Per Paper", subtitle = glue::glue("Dropping {nrow(x$papers[hours_spent >= 10])} Outliers"))
+
+    l$iters_p = ggplot(x$papers, aes(x = arrival_date, y = iterations_paper)) + geom_point(size = 0.5) +
+        geom_line(data = ip, aes(x = arrival_date, y = iterations_paper)  , color = "red",size = 1) + theme_bw() +
+        scale_x_date("Arrival Date",limits = c(as.Date("2020-01-01"), NA),breaks = seq(as.Date("2020-01-01"), as.Date("2023-10-01"), by = "6 months") ,date_labels = "%Y-%m") +
+        ggtitle("Iterations Per Paper")
+
+
+    l
+}
+
+
+plot_proctime <- function(x, by = "Iteration"){
+
+    if (by == "Iteration"){
+        labs = c("Assignment","Replication","Decision", "Resubmission")
+        mr = data.table::melt(x, id.vars = c("arrival_date","ms","round"),
+                              measure.vars = c("time_assign",
+                                               "cumtime_replication",
+                                               "cumtime_decision",
+                                               "cumtime_resubmission"),
+                              value.name =  "Days")
+        mr = mr[Days < 50 & arrival_date > "2020-01-01" & Days >= 0]
+
+        # mean hours by arrival date
+        h = x[, .(hours = mean(hours_spent, na.rm = TRUE)), by = arrival_date]
+        h[, smooth := data.table::frollmean(hours,n = 30,na.rm = TRUE)]
+
+        hline_v = 15
+
+
+
+    } else {
+        # by paper
+        labs = c("Assignment","Replication","Decision", "Resubmission")
+        mr = data.table::melt(x, id.vars = c("arrival_date","ms"),
+                              measure.vars = c("time_assign_paper",
+                                               "time_replication_paper",
+                                               "time_decision_paper",
+                                               "time_resubmission_paper"),
+                              value.name =  "Days")
+        mr = mr[Days < 60 & arrival_date > "2020-01-01" & Days >= 0]
+
+        hline_v = 25
+
+    }
+    mr[, arrival_date := as.Date(arrival_date)]
+    setkey(mr, arrival_date,ms)
+
+    # rejection
+    haz = mr[, .(hazard = mean(Days,na.rm = TRUE)), by = .(arrival_date, variable)]
+    haz[, smooth := data.table::frollmean(hazard,n = 40,na.rm = TRUE), by = .(variable)]
+
+    # mean hours spent by arrival date
+    # h = x[, .(hours = mean(hours_spent, na.rm = TRUE)), by = arrival_date]
+    # h[, smooth := data.table::frollmean(hours,n = 30,na.rm = TRUE)]
+
+    # base plot
+    p = ggplot(mr, aes(x = arrival_date,y= Days, color = variable)) + geom_point(size = 0.5) +
+        geom_line(data = haz, aes(x = arrival_date, y = smooth, color = variable),size = 1)
+
+
+
+    #days
+    days = p + ggplot2::theme_bw() + ggplot2::theme(legend.position = "bottom") +
+        ggplot2::scale_color_discrete(name = "Time Until:",
+                           labels = labs) +
+        scale_x_date("Arrival Date",limits = c(as.Date("2020-01-01"), NA),breaks = seq(as.Date("2020-01-01"), as.Date("2023-10-01"), by = "6 months") ,date_labels = "%Y-%m") +
+        geom_vline(xintercept = as.Date("2023-07-01")) +
+        geom_hline(yintercept = hline_v, linetype = "dashed") +
+        ggplot2::scale_y_continuous(breaks = c(0,15,20,25,40,60)) +
+        ggtitle(glue::glue("Processing Time By {by}"))
+
+    # #hours
+    # l$hours = p + ggplot2::theme_bw() + ggplot2::theme(legend.position = "bottom") +
+    #     scale_color_discrete(name = "Time Until:",
+    #                          labels = labs) +
+    #     scale_x_date("Arrival Date",breaks = seq(as.Date("2020-01-01"), as.Date("2023-10-01"), by = "6 months") ,date_labels = "%Y-%m") +
+    #     geom_vline(xintercept = as.Date("2023-07-01")) +
+    #     geom_hline(yintercept = 15, linetype = "dashed") +
+    #     ggtitle(glue::glue("Processing Time By {by}"))
+    days
+}
+
+#' Plot number and share of software used
+#'
+plot_software <- function(y){
+    y[, completed := status %in% c("AP","NT", "P", "p"), by = ms]
+    x = y[(completed), .(date_completed, software)]
+
+    x[ , stata := grepl("STATA", software, ignore.case = TRUE)]
+    x[ , R := grepl("\\bR\\b|RSTUDIO|RSTAN", software, ignore.case = TRUE)]
+    x[ , python := grepl("PYTHON", software, ignore.case = TRUE)]
+    x[ , matlab := grepl("MATLAB", software, ignore.case = TRUE)]
+    x[ , GAMS := grepl("GAMS", software, ignore.case = TRUE)]
+    x[ , ArcGIS_QGIS := grepl("ARCGIS|QGIS", software, ignore.case = TRUE)]
+    x[ , fortran := grepl("FORTRAN", software, ignore.case = TRUE)]
+    x[ , cpp := grepl("C\\+\\+", software, ignore.case = TRUE)]
+    x[ , mathematica := grepl("MATHEMATICA|WOLFRAM", software, ignore.case = TRUE)]
+    x[ , julia := grepl("JULIA", software, ignore.case = TRUE)]
+    x[ , excel := grepl("EXCEL", software, ignore.case = TRUE)]
+    x[ , SAS := grepl("SAS", software, ignore.case = TRUE)]
+    x[ , dynare := grepl("DYNARE", software, ignore.case = TRUE)]
+    x[ , ztree := grepl("ZTREE|Z\\-TREE", software, ignore.case = TRUE)]
+    x[ , GAUSS := grepl("GAUSS", software, ignore.case = TRUE)]
+
+    x[, completed_quarter := zoo::as.yearqtr(date_completed)]
+    x[ , software :=NULL]
+    x[ , date_completed :=NULL]
+
+    x[, lapply(.SD, mean,na.rm = TRUE), by = completed_quarter]
+    d = x[, lapply(.SD, sum,na.rm = TRUE), .SDcols = stata:ztree]
+    dd = data.table(Papers = t(d), Software = names(d))
+    data.table::setnames(dd, "Papers.V1", "Papers")
+    dd = rbind(dd, data.table(Papers = dd[Papers < 6, sum(Papers)], Software = "Other"))
+    dd = dd[Papers >=6]
+    dd = dd[order(Papers, decreasing = TRUE)]
+    dd[, Share := Papers / sum(Papers)]
+    dd = rbind(dd[-which(Software == "Other")], dd[which(Software == "Other")])
+
+    data.table::setcolorder(dd, neworder = c("Software","Papers","Share"))
+    dd
+}
+
+#' Plot Data Statements
+#'
+plot_DS <- function(x){
+
+    # group sub cases of DS together somehow:
+    x[, ds_A := grepl("^[^(T|S|P)]*[A][^(T|S|P)]*$", data_statement)]
+    x[, ds_AS := grepl("^[^(P|T)]*A[^(P|T)]*$", data_statement) * grepl("^[^(P|T)]*S[^(P|T)]*$", data_statement)]
+    x[, ds_AT := grepl("^[^(P|S)]*A[^(P|S)]*$", data_statement) * grepl("^[^(P|S)]*T[^(P|S)]*$", data_statement)]
+    x[, ds_T := grepl("^[^(A|S|P)]*[T][^(A|S|P)]*$", data_statement)]
+    x[, ds_P := grepl("P", data_statement)]  # any P
+    # x[, ds_AP := grepl("^[^(P|T)]*A[^(P|T)]*$", data_statement) * grepl("^[^(P|T)]*S[^(P|T)]*$", data_statement)]
+    # x[, ds_ATS := grepl("A", data_statement) * grepl("S", data_statement) * grepl("T", data_statement)]
+    # x[, ds_ATP := grepl("A", data_statement) * grepl("P", data_statement) * grepl("T", data_statement)]
+
+    x[, completed := status %in% c("AP","NT", "P", "p"), by = ms]
+    x[, arrival_quarter := zoo::as.yearqtr(arrival_date)]
+
+    pd = x[(completed), lapply(.SD, mean,na.rm = TRUE), by = arrival_quarter,.SDcols = ds_A:ds_P]
+    m = melt(pd, id.vars = "arrival_quarter", value.name = "Share")
+
+    facet_names = c(
+        ds_A = "Data 100% Available",
+        ds_AS = "Some Data, Some Simulation",
+        ds_AT = "Some Data, Some Temp. Access",
+        ds_T = "Temp. Access Only",
+        ds_P = "Partial Replication Only"
+
+    )
+    ggplot(m, aes(x = arrival_quarter, y = Share)) + geom_line() +
+        ggplot2::facet_wrap(~variable,ncol = 1, labeller = ggplot2::as_labeller(facet_names)) + theme_bw()
+}
+
+
+
+plot_hazard <- function(x){
+    x[, round := factor(sround)]
+    x[, arrival_date := as.Date(arrival_date)]
+
+    m = x[, .(mh = mean(hazard_smooth, na.rm = TRUE)),by = round]
+
+    # data.table::setnames(x, "hazard_smooth", "hazard")
+    ggplot(x, aes(x = arrival_date, y = hazard_smooth, color = round)) + geom_line() +
+        ggplot2::theme_bw() + ggplot2::theme(legend.position = "bottom") +
+        geom_vline(xintercept = as.Date("2023-07-01"),size = 1) +
+        geom_line(aes(y = ave(hazard_smooth, round, FUN = function(x){mean(x,na.rm = TRUE)})),size = 1) +
+        ggtitle("Rejection Hazard by Round of Iteration")
 
 }
